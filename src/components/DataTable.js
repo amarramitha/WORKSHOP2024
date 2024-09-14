aaa594946b6077a3400e7795d624c0d986a07ea2
Amarramitha <amarra.mitha@gmail.com>
2024-09-14 08:58:18 +0700

    menampilkan image

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,20 +1,29 @@
+
+
 import React from 'react';
 
 const DataTable = ({ config, data }) => {
   return (
-    <table class="min-w-full bg-white shadow-md rounded">
+    <table className="min-w-full bg-white shadow-md rounded">
       <thead>
         <tr>
           {config.map((column) => (
-            <th class="py-3 px-6 bg-gray-200 text-left text-xs font-medium text-black uppercase tracking-wider" key={column.id}>{column.label}</th>
+            <th key={column.id} className="py-3 px-6 bg-gray-200 text-left text-xs font-medium text-black uppercase tracking-wider">{column.label}</th>
           ))}
         </tr>
       </thead>
       <tbody>
         {data.map((row, rowIndex) => (
-          <tr class="bg-gray-50" key={rowIndex}>
+          <tr key={rowIndex} className="border-t">
             {config.map((column) => (
-              <td class="py-4 px-6 border-b border-gray-200 text-sm text-black" key={column.id}>{row[column.id]}</td>
+              <td key={column.id} className="py-4 px-6 border-b border-gray-200 text-sm text-black">
+                {/* Cek jika kolom adalah 'img' dan tampilkan gambar */}
+                {column.id === 'img' && row[column.id] ? (
+                  <img src={row[column.id]} alt={row.title} className="h-16 w-16 object-cover rounded-lg" />
+                ) : (
+                  row[column.id]
+                )}
+              </td>
             ))}
           </tr>
         ))}
--- src/data/product.js
+++ src/data/product.js
@@ -1,6 +1,18 @@
 export const productData = [
-    { title: "Product 1", desc: "This is a great product.", price: "$29.99", img: "", serial: "123456789", createdby: "Joh Doe" },
-    { title: "Product 2", desc: "Another amazing product.", price: "$39.99", img: "", serial: "987654321", createdby: "Jane Smith" },
+    { title: "Product 1", 
+      desc: "This is a great product.", 
+      price: "$29.99", 
+      img: "https://miro.medium.com/v2/resize:fit:1200/0*uBmjsSha3kUWjJ_J.jpg", 
+      serial: "123456789", 
+      createdby: "Joh Doe" 
+    },
+    { title: "Product 2", 
+      desc: "Another amazing product.", 
+      price: "$39.99", 
+      img: "https://static.scientificamerican.com/sciam/cache/file/32665E6F-8D90-4567-9769D59E11DB7F26_source.jpg", 
+      serial: "987654321", 
+      createdby: "Jane Smith" 
+    },
   ];
 
 //   [
